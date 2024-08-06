4f6e627e5b58b9e43aa86591b5a1af0a61e51bca
sudiptamax <sudiptamax@outlook.com>
2024-08-06 22:54:59 +0530

    Add files via upload
    Reactive Form implemented for Create-update page !!

--- src/app/components/create-update-blog/create-update-blog.component.ts
+++ src/app/components/create-update-blog/create-update-blog.component.ts
@@ -2,32 +2,39 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } fr
 import { ActivatedRoute, Router } from '@angular/router';
 import { BlogService } from '../../services/blog.service';
 import { Blog } from '../../models/blog.model';
-
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-update-blog',
   templateUrl: './create-update-blog.component.html',
   styleUrls: ['./create-update-blog.component.css']
 })
-export class CreateUpdateBlogComponent implements OnInit {
+export class CreateUpdateBlogComponent implements OnInit, AfterViewInit {
   @ViewChild('usernameInput') usernameInput!: ElementRef;
-  blog: Blog = { id: '', username: '', text: '', dateCreated: new Date() };
+  blogForm: FormGroup;
   isEdit: boolean = false;
 
   constructor(
+    private fb: FormBuilder,
     private blogService: BlogService,
     private router: Router,
     private route: ActivatedRoute,
     private renderer: Renderer2
-  ) {}
+  ) {
+    this.blogForm = this.fb.group({
+      id: [''],
+      username: ['', Validators.required],
+      text: ['', Validators.required],
+      dateCreated: [new Date()]
+    });
+  }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.isEdit = true;
       this.blogService.getBlogById(id).subscribe(blog => {
-        this.blog = blog;
+        this.blogForm.patchValue(blog);
       });
     }
   }
@@ -38,17 +45,21 @@ export class CreateUpdateBlogComponent implements OnInit {
     }
   }
 
-saveBlog(): void {
-  if (this.isEdit) {
-    this.blogService.updateBlog(this.blog.id, this.blog).subscribe(
-      () => this.router.navigate(['/list']),
-      error => console.error('Error updating blog:', error)
-    );
-  } else {
-    this.blogService.createBlog(this.blog).subscribe(
-      () => this.router.navigate(['/list']),
-      error => console.error('Error creating blog:', error)
-    );
+  saveBlog(): void {
+    if (this.blogForm.valid) {
+      const blogData: Blog = this.blogForm.value;
+      if (this.isEdit) {
+        this.blogService.updateBlog(blogData.id, blogData).subscribe(
+          () => this.router.navigate(['/list']),
+          error => console.error('Error updating blog:', error)
+        );
+      } else {
+        this.blogService.createBlog(blogData).subscribe(
+          () => this.router.navigate(['/list']),
+          error => console.error('Error creating blog:', error)
+        );
+      }
+    }
   }
 }
-}
+
